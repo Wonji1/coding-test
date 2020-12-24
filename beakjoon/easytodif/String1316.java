a88bbe48f80c35fcbc35f5188be8f883ba4478cf
Wonji1 <53804188+Wonji1@users.noreply.github.com>
2020-12-24 17:45:18 +0900

    20.12.24

--- beakjoon/easytodif/String1316.java
+++ beakjoon/easytodif/String1316.java
@@ -7,13 +7,31 @@ import java.io.InputStreamReader;
 public class String1316 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = br.read();
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
         for(int i =0;i<n;i++){
             String s = br.readLine();
             int[] arr = new int[26];
-            for(int j=0;j<s.length();j++){
+            boolean b = true;
+            for(int j=0;j<s.length()-1;j++){
+                if(arr[s.charAt(j)-'a'] == -1){
+                    b =false;
+                    break;
+                }
+                if(s.charAt(j) != s.charAt(j+1)){
+                    arr[s.charAt(j)-'a'] = -1;
+                }
+            }
+            if(arr[s.charAt(s.length()-1) -'a'] == -1){
+                b=false;
+            }
+            if (b){
+                cnt+=1;
+            }else if(b && s.charAt(s.length()-1) != s.charAt(s.length()-2)){
+                cnt+=1;
             }
         }
+        System.out.println(cnt);
     }
     
 }
