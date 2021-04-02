309e33babae3be00ab2078f0d60fd81f88ffb650
Wonji1 <53804188+Wonji1@users.noreply.github.com>
2021-04-02 23:09:40 +0900

    21.04.02

---
+++ programmers/programmers_basic/dp2.java
@@ -0,0 +1,11 @@
+public class dp2 {
+    public static int solution(int[][] triangle) {
+        int answer = 0;
+        
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[][] triangle = {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
+        System.out.println(solution(triangle));
+    }
+}
